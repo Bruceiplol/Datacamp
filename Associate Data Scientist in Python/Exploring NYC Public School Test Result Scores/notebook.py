75276a313d199c52441fcd2cebcef98a7bc3fac4
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2024-03-24 01:02:19 +0800

    Create notebook.py

---
+++ Associate Data Scientist in Python/Exploring NYC Public School Test Result Scores/notebook.py
@@ -0,0 +1,11 @@
+# Re-run this cell 
+import pandas as pd
+
+# Read in the data
+schools = pd.read_csv("schools.csv")
+
+# Preview the data
+schools.head()
+
+# Start coding here...
+# 1. Which NYC schools have the best math results?
