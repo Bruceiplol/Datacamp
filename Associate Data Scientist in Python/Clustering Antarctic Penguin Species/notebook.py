d32e90ae43a7038ed94242bb916b93c6158b7041
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2024-03-29 15:57:11 +0800

    Update notebook.py

--- Associate Data Scientist in Python/Clustering Antarctic Penguin Species/notebook.py
+++ Associate Data Scientist in Python/Clustering Antarctic Penguin Species/notebook.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 # Loading and examining the dataset
-penguins_df = pd.read_csv("data/penguins.csv")
+penguins_df = pd.read_csv("./penguins.csv")
 print(penguins_df.head())
 print(penguins_df.info())
 print(penguins_df.isna().sum())
