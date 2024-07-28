3765dd4fc83eb1bdd1e6ec6b6d7470596a214f21
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2024-07-28 20:24:34 +0900

    Update instructions.py

--- Data Science in Python/When Was the Golden Era of Video Games?/instructions.py
+++ Data Science in Python/When Was the Golden Era of Video Games?/instructions.py
@@ -11,3 +11,40 @@ Find the ten years with the highest average critic score, where at least four ga
 Find the years where critics and users broadly agreed that the games released were highly rated. Specifically, return the years where the average critic score was over 9 OR the average user score was over 9. The pre-computed average critic and user scores per year are stored in users_avg_year_rating and critics_avg_year_rating tables respectively. The query should return the following columns: year, num_games, avg_critic_score, avg_user_score, and diff. The diff column should be the difference between the avg_critic_score and avg_user_score. The table should be ordered by the year in ascending order, save this as a DataFrame named golden_years.
 
 Note: Please also ensure that you do not change the names of the DataFrames that the three query results will be saved as - creating new cells in the workbook will rename the DataFrame. Make sure that your final solutions use the names provided: best_selling_games, critics_top_ten_years, and golden_years.
+
+### `game_sales` table
+
+| Column | Definition | Data Type |
+|-|-|-|  
+|name|Name of the video game|`varchar`|
+|platform|Gaming platform|`varchar`|
+|publisher|Game publisher|`varchar`|
+|developer|Game developer|`varchar`|
+|games_sold|Number of copies sold (millions)|`float`|
+|year|Release year|`int`|
+
+### `reviews` table
+
+| Column | Definition | Data Type |
+|-|-|-|
+|name|Name of the video game|`varchar`|  
+|critic_score|Critic score according to Metacritic|`float`|
+|user_score|User score according to Metacritic|`float`|
+
+
+### `users_avg_year_rating` table
+
+| Column | Definition | Data Type |
+|-|-|-|
+|year| Release year of the games reviewed |`int`|  
+|num_games| Number of games released that year |`int`|
+|avg_user_score| Average score of all the games ratings for the year |`float`|
+
+### `critics_avg_year_rating` table
+
+| Column | Definition | Data Type |
+|-|-|-|
+|year| Release year of the games reviewed |`int`|  
+|num_games| Number of games released that year |`int`|
+|avg_critic_score| Average score of all the games ratings for the year |`float`|
+
